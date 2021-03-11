faca15e0f8d775cc00615278451f212b018736ed
DuaaMahya <temoka48@gmail.com>
2021-03-11 11:27:55 +0300

    Revert "commit branch"
    This reverts commit e7bd78a59fd1926e15e8c53c17ae9399e762e54e.

---
+++ ChatApp/Controllers/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  ChatApp
+//
+//  Created by Dua Almahyani on 03/02/2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
